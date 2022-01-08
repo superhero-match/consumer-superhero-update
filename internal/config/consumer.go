77110455aa07320a0493dbb934ea0d90752f6cf8
Nikolajus Karpovas <superheromatch@gmail.com>
2022-01-08 20:10:35 +0200

    Update config package

--- internal/config/consumer.go
+++ internal/config/consumer.go
@@ -15,7 +15,7 @@ package config
 
 // Consumer holds the configuration values for the Kafka consumer.
 type Consumer struct {
-	Brokers []string `env:"KAFKA_BROKERS" yaml:"brokers" default:"[192.168.0.64:9092]"`
+	Brokers []string `env:"KAFKA_BROKERS" yaml:"brokers" default:"[192.168.1.229:9092]"`
 	Topic   string   `env:"KAFKA_UPDATE_MUNICIPALITY_SUPERHERO_TOPIC" yaml:"topic" default:"update.municipality.superhero"`
 	GroupID string   `env:"KAFKA_CONSUMER_UPDATE_SUPERHERO_GROUP_ID" yaml:"group_id" default:"consumer.update.superhero.group"`
 }
